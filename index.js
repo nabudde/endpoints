d9bd43e80106930fceac25feb886a0187868528a
nabudde <kaseddeblairebelinda@gmail.com>
2019-07-23 10:11:56 +0300

    add index.js file

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// const Joi = require('joi');
 const express=require("express");
 const bodyParser = require('body-parser')
 const app=express();
@@ -7,26 +8,17 @@ bodyParser.json(app)
 
 let signup=[];
 let signin=[];
-let createTrip=[];
-let cancelTrip=[
-    {
-        "id" : "1",
-        "seating_capacity" : "60",
-        "bus_license_number" : "UBB23",
-        "origin" : "Kampala", 
-        "destination" : "Kigali",
-        "trip_date" : "01/01/2020",
-        "fare" : "100000"
-    }
-];
+let Trips=[];
+let bookings=[];
 
-app.post('/api/v',(req, res)=>{
+
+app.post('/api/v1/signup',(req, res)=>{
     if(!req.body.Email){
         res.status(400).send("email is required");
         return;
     }
     if(!req.body.Password){
-        res.status(400).send("pasword is required");
+        res.status(400).send("password is required");
         return;
     }
     if(!req.body.first_name){
@@ -41,7 +33,7 @@ app.post('/api/v',(req, res)=>{
         res.status(400).send("this field required");
         return;    
     }
-
+    
     const create_signup={
         Email:req.body.Email,
         Password:req.body.Password,
@@ -54,8 +46,8 @@ app.post('/api/v',(req, res)=>{
     res.send(create_signup);
     console.log(create_signup);
 });
-/*
-app.post('/api/v/v1',(req, res)=>{
+
+app.post('/api/v1/signin',(req, res)=>{
     if(!req.body.Email){
         res.status(400).send("email is required");
         return;
@@ -72,11 +64,11 @@ app.post('/api/v/v1',(req, res)=>{
     res.send(create_signin);
     console.log(create_signin);   
 });
-*/
 
-app.post('api/v/v1',(req,res)=>{
+
+app.post('/api/v1/create',(req,res)=>{
     const create_trip={
-        id :req.body.id,
+        trip_id :req.body.trip_id,
         seating_capacity : req.body.seating_capacity,
         bus_license_number : req.body.bus_license_number,
         origin : req.body.origin, 
@@ -84,13 +76,46 @@ app.post('api/v/v1',(req,res)=>{
         trip_date : req.body.trips_date,
         fare : req.body.fare
     };
-    createTrip.push(create_trip);
+    Trips.push(create_trip);
     res.send(create_trip);
     console.log(create_trip);   
 });
-app.get('api/v/v1/v2/v3',(req,res)=>{
-    res.send(cancelTrip);
-    console.log(createTrip);  
+
+app.get('/api/v1/trips',(res,req)=>{
+    req.send(Trips);
+
+});
+app.delete('/api/v1/delete/:trip_id',(req,res)=>{
+    const tripDelete=Trips.find(t=>t.trip_id==(req.params.trip_id));
+    if (!tripDelete) res.status(404).send('the provided trip_id is not available');
+    const index=Trips.indexOf(tripDelete)
+    Trips.splice(index,1);
+    res.send(tripDelete);
+});
+app.get('/api/v1/trips/:trip_id',(req,res)=>{
+    const specificTrip=Trips.find(t=>t.trip_id==(req.params.trip_id));
+    if (!specificTrip) res.status(404).send('the provided trip_id is not available');
+    res.send(specificTrip);
+});
+app.post('/api/v1/bookaseat',(req, res)=>{
+    const bookASeat={
+        id : req.body.id,
+        trip_id : req.body.trip_id,
+        user_id : req.body.user_id,
+        created_on : req.body.created_on
+    }
+    bookings.push(bookASeat);
+    res.send(bookASeat);
+});
+app.get('/api/v1/allbookings',(req, res)=>{
+    res.send(bookings);
+});
+app.delete('/api/v1/deletebooking/:id',(req,res)=>{
+    const deleteBooking=bookings.find(b=>b.id==(req.params.id));
+    if(!deleteBooking) res.status(404).send('provided id is not available');
+    const Index=bookings.indexOf(deleteBooking);
+    bookings.splice(Index,1);
+    res.send(deleteBooking);
 })
 
 
